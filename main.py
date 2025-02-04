525700c9213df0b5332457c3c8b964c88740fc4f
Brodzman <thbrodz@gmail.com>
2025-02-04 18:52:24 +1100

    Add the ability to draw to the window

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from window import Window
+from window import *
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(line, fill_colour='black')
     win.wait_for_close()
 
 main()
