99b4d20b209c85b3174c7c1e915b91cafdf11e59
Brodzman <thbrodz@gmail.com>
2025-02-04 20:58:17 +1100

    Add a drawing line from cell to cell

--- window.py
+++ window.py
@@ -53,15 +53,38 @@ class Cell:
         self.win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self.win is None:
+            return
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
         if self.has_left_wall == True:
-            line=Line(Point(x1, y1), Point(x1, y2))
+            line = Line(Point(x1, y1), Point(x1, y2))
             self.win.draw_line(line)
         if self.has_bottom_wall == True:
-            line=Line(Point(x1, y2), Point(x2, y2))
+            line = Line(Point(x1, y2), Point(x2, y2))
             self.win.draw_line(line)
         if self.has_right_wall == True:
-            line=Line(Point(x2, y1), Point(x2, y2))
+            line = Line(Point(x2, y1), Point(x2, y2))
             self.win.draw_line(line)
         if self.has_top_wall == True:
-            line=Line(Point(x1, y1), Point(x2, y1))
+            line = Line(Point(x1, y1), Point(x2, y1))
             self.win.draw_line(line)
+        return x1, y1, x2, y2
+
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self.x2 - self.x1) // 2
+        x_center = half_length + self.x1
+        y_center = half_length + self.y1
+
+        half_length2 = abs(to_cell.x2 - to_cell.x1) // 2
+        x_center2 = half_length2 + to_cell.x1
+        y_center2 = half_length2 + to_cell.y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self.win.draw_line(line, fill_color)
